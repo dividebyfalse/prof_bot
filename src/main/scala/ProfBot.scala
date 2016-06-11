11c7350d8ac4a7138eec5240866caf91964ad2ef
Evmap <xdima24@gmail.com>
2016-06-11 21:24:38 +0800

    Are you working??REALLY?

---
+++ src/main/scala/ProfBot.scala
@@ -0,0 +1,81 @@
+import scala.io._
+import scala.collection.mutable.ListBuffer
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+object ProfBot extends App {
+  val access_token = "access_token=228e1561f8bfbc97b7b5ebf5b94baadcb94c9d7491e96ac7ccf273fce3dc99030d2263349f29db3fa0406"
+  val groups_resp = parse(vk_req("https://api.vk.com/method/groups.get?filter=moder&"+ "v=5.52&"+ access_token)) \ "response"
+  val groups_items = groups_resp \ "items"
+  val groups_count = (groups_resp \ "count").values.toString.toInt
+  println(groups_resp)
+
+  var group_list = new ListBuffer[Int]()
+
+  for(i <- 0 to groups_count-1) {
+    group_list += groups_items(i).values.toString.toInt
+  }
+  println(group_list.length)
+  // val url_repost = "https://api.vk.com/method/wall.repost?object=wall-22122847_25237&group_id=12998578&access_token=d14f404d51d4960b2032fabd99fc3d6f271751dd0a0fdfc1b27d2fb96b2285009d2839600f8d02b36ea59"
+  // print(Source.fromURL(createUrlRepost("wall-22122847_25237")).mkString)
+  var i = 0
+  while(i<100) {
+    Thread.sleep(1500)
+    CheckUpdate
+    i = i + 1
+  }
+  def CheckUpdate = {
+    val dialogs = GetDialogs
+    val d_count = dialogs(0).values.toString.toInt
+    var i:Int = 0
+    while(i < d_count) {
+      i = i + 1
+      if ((dialogs(i) \ "read_state").values == 0 && (dialogs(i) \ "out").values == 0){
+        MarkAsRead(dialogs(i) \ "mid")
+        if (dialogs(i) \ "attachment" != JNothing && (dialogs(i) \ "attachment" \ "type").values.toString == "wall"){
+          for(j <- 0 to group_list.length-1) {         
+            val group_from_id = (dialogs(i) \ "attachment" \ "wall" \ "from_id").values.toString.toInt
+            if(group_list(0) != group_from_id){
+              just_do_it(dialogs(i) \ "attachment", group_list(j))
+            }
+          }
+        } else {
+          sendMessage("Не_пиши_мне_больше!_"+(dialogs(i) \ "mid").values.toString,(dialogs(i) \ "uid").values.toString.toInt)
+        }
+      }
+    }
+  }
+
+  def sendMessage(message:String, to_id: Int):String = {
+    vk_req("https://api.vk.com/method/messages.send?message="+ message + "&user_id="+ to_id + "&" + access_token)
+  }
+
+  def MarkAsRead(message_id: JValue) = {
+    val id = message_id.values
+    vk_req("https://api.vk.com/method/messages.markAsRead?message_ids="+ id + "&" + access_token)
+  }
+  def GetDialogs = {
+    val resp = vk_req("https://api.vk.com/method/messages.getDialogs?" +access_token)
+    parse(resp) \ "response"
+  }
+
+  def just_do_it(resp_attach: JValue, group_id: Int):String = {
+    vk_req(createUrlRepost(createWallObject(resp_attach)+"&group_id="+ group_id))
+  }
+
+  def createWallObject(readstate: JValue):String = {
+    val w_id = (readstate \ "wall" \ "id").values.toString.toInt
+    val w_group = (readstate \ "wall" \ "from_id").values.toString.toInt
+    "wall"+ w_group + "_" + w_id
+  }
+
+  def createUrlRepost(wall_object: String):String = {
+    "https://api.vk.com/method/wall.repost?object=" + wall_object + "&"+ access_token
+  }
+
+  def vk_req(req: String): String = {
+    Thread.sleep(500)
+    Source.fromURL(req).mkString
+  }
+
+}
