32dc3c9d8e00c1139bb5953d2bb4c5b0a35d987c
Evmap <xdima24@gmail.com>
2016-06-16 14:11:26 +0800

    Optimization

--- src/main/scala/ProfBot.scala
+++ src/main/scala/ProfBot.scala
@@ -1,28 +1,28 @@
 import scala.io._
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
 import org.json4s._
 import org.json4s.native.JsonMethods._
 
 object ProfBot extends App {
   val access_token = "access_token=228e1561f8bfbc97b7b5ebf5b94baadcb94c9d7491e96ac7ccf273fce3dc99030d2263349f29db3fa0406"
-  val groups_resp = parse(vk_req("https://api.vk.com/method/groups.get?filter=moder&"+ "v=5.52&"+ access_token)) \ "response"
-  val groups_items = groups_resp \ "items"
-  val groups_count = (groups_resp \ "count").values.toString.toInt
-  println(groups_resp)
-
-  var group_list = new ListBuffer[Int]()
-
-  for(i <- 0 to groups_count-1) {
-    group_list += groups_items(i).values.toString.toInt
-  }
-  println(group_list.length)
+  def group_list = getGroupList
   // val url_repost = "https://api.vk.com/method/wall.repost?object=wall-22122847_25237&group_id=12998578&access_token=d14f404d51d4960b2032fabd99fc3d6f271751dd0a0fdfc1b27d2fb96b2285009d2839600f8d02b36ea59"
   // print(Source.fromURL(createUrlRepost("wall-22122847_25237")).mkString)
-  var i = 0
-  while(i<100) {
+  def getGroupList = {
+    val group_list = new ArrayBuffer[Int]()
+    val groups_resp = parse(vk_req("https://api.vk.com/method/groups.get?filter=moder&"+ "v=5.52&"+ access_token)) \ "response"
+    val groups_items = groups_resp \ "items"
+    val groups_count = (groups_resp \ "count").values.toString.toInt
+    for(i <- 0 until groups_count) {
+      group_list += groups_items(i).values.toString.toInt
+    }
+    group_list
+  }
+
+  while(true) {
+    println(group_list)
     Thread.sleep(1500)
     CheckUpdate
-    i = i + 1
   }
   def CheckUpdate = {
     val dialogs = GetDialogs
@@ -33,14 +33,14 @@ object ProfBot extends App {
       if ((dialogs(i) \ "read_state").values == 0 && (dialogs(i) \ "out").values == 0){
         MarkAsRead(dialogs(i) \ "mid")
         if (dialogs(i) \ "attachment" != JNothing && (dialogs(i) \ "attachment" \ "type").values.toString == "wall"){
-          for(j <- 0 to group_list.length-1) {         
+          for(elem <- group_list) {
             val group_from_id = (dialogs(i) \ "attachment" \ "wall" \ "from_id").values.toString.toInt
             if(group_list(0) != group_from_id){
-              just_do_it(dialogs(i) \ "attachment", group_list(j))
+              just_do_it(dialogs(i) \ "attachment", elem)
             }
           }
         } else {
-          sendMessage("Не_пиши_мне_больше!_"+(dialogs(i) \ "mid").values.toString,(dialogs(i) \ "uid").values.toString.toInt)
+          sendMessage("Держитесь_там!_Всего_доброго_и_хорошего_настроения!"+(dialogs(i) \ "mid").values.toString,(dialogs(i) \ "uid").values.toString.toInt)
         }
       }
     }
